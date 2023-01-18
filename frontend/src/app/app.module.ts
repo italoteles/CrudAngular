d2dcefc2017cc5ba8b549b32eafce788936c095f
italoteles <italop.teles@gmail.com>
2023-01-18 13:22:48 -0300

    Componente delete criado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,6 +30,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
 
 import localePt from '@angular/common/locales/pt';
 import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -42,7 +44,9 @@ registerLocaleData(localePt);
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent
     
   ],
   imports: [
