2726dff9b816de8cb620bff3744233215f34d201
italoteles <italop.teles@gmail.com>
2023-01-19 13:31:06 -0300

    Projeto finalizado

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HeaderService } from './header.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private headerService : HeaderService){}
+
+  get title(){
+    return this.headerService.headerData.title;
+  }
+  get icon(){
+    return this.headerService.headerData.icon;
+  }
+  get routeUrl(){
+    return this.headerService.headerData.routeUrl;
+  }
 }
